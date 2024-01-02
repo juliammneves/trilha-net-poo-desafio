fc08c73752d044578c60a872c0414ed5e455ec93
Júlia <juliammneves@gmail.com>
2024-01-02 13:06:56 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Nokia n = new Nokia("12345", "x12", "111111", 64);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nIphone:");
+Iphone i = new Iphone("12345", "14 pro Max", "2222222", 128);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Instagram");
